dcbc33811e78c60f38c0360bf750a52ee37d41b1
phamtuan11520011 <phamtuan1152001@gmail.com>
2023-04-18 18:06:52 +0700

    fix project  source

---
+++ src/Modules/StartGame/constants.js
@@ -0,0 +1,27 @@
+export const TILE_STATUS = {
+  HIDDEN: "hidden",
+  MINE: "mine",
+  NUMBER: "number",
+  MARKED: "marked",
+};
+
+export const BOARD_SIZE = 10;
+export const NUMBER_OF_MINES = 10;
+
+export const LEVEL_MINE = [
+  {
+    id: 1,
+    label: "Easy",
+    value: 5,
+  },
+  {
+    id: 2,
+    label: "Medium",
+    value: 10,
+  },
+  {
+    id: 1,
+    label: "Hard",
+    value: 15,
+  },
+];
