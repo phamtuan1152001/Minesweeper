0ed42d8c94e139c1c8f6f9fca5e61f6dbbdc9c5a
phamtuan11520011 <phamtuan1152001@gmail.com>
2023-05-30 00:29:43 +0700

    fix

--- src/Modules/Ranking/index.js
+++ src/Modules/Ranking/index.js
@@ -1,18 +1,25 @@
-import React, {useState} from 'react'
-import {data} from './users'
-import "./table.css"
-import {FaArrowUp, FaArrowDown} from "react-icons/fa"
+import React, { useEffect, useState } from "react";
+import { data } from "./users";
+import "./table.css";
+import { FaArrowUp, FaArrowDown } from "react-icons/fa";
+
+import { LEVEL_MINE } from "../StartGame/constants";
 
 function Ranking() {
-  const [users, setUsers] = useState("");
-  const [sorted, setSorted] = useState({sorted: "id", reversed: false});
+  const [users, setUsers] = useState([]);
+  const [sorted, setSorted] = useState({ sorted: "id", reversed: false });
   const [searchPhase, setSearchPhase] = useState("");
 
-  const sortById =() => {
-    setSorted({sorted: "id", reversed: !sorted.reversed});
+  useEffect(() => {
+    const list = JSON.parse(localStorage.getItem("listUserPlay"));
+    setUsers(list);
+  }, []);
+  console.log("users", users);
+  const sortById = () => {
+    setSorted({ sorted: "id", reversed: !sorted.reversed });
     const usersCopy = [...users];
-    usersCopy.sort((user1, user2) =>{
-      if(sorted.reversed){
+    usersCopy.sort((user1, user2) => {
+      if (sorted.reversed) {
         return user1.id - user2.id;
       }
       return user2.id - user1.id;
@@ -20,14 +27,14 @@ function Ranking() {
     setUsers(usersCopy);
   };
 
-  const sortByName =() => {
-    setSorted({sorted: "name", reversed: !sorted.reversed});
+  const sortByName = () => {
+    setSorted({ sorted: "name", reversed: !sorted.reversed });
     const usersCopy = [...users];
-    usersCopy.sort((user1, user2) =>{
+    usersCopy.sort((user1, user2) => {
       const fullnameA = `${user1.first_name} ${user2.last_name}`;
       const fullnameB = `${user2.first_name} ${user1.last_name}`;
 
-      if (sorted.reversed){
+      if (sorted.reversed) {
         return fullnameB.localeCompare(fullnameA);
       }
       return fullnameA.localeCompare(fullnameB);
@@ -37,34 +44,35 @@ function Ranking() {
 
   const search = (event) => {
     const matchedUsers = data.filter((user) => {
-      return`${user.first_name} ${user.last_name}`
-      .toLowerCase()
-      .includes(event.target.value.toLowerCase());
+      return `${user.first_name} ${user.last_name}`
+        .toLowerCase()
+        .includes(event.target.value.toLowerCase());
     });
     setUsers(matchedUsers);
     setSearchPhase(event.target.value);
-  }
-
- const renderArrow = () => {
-    if (sorted.reversed){
-      return <FaArrowUp/>
-    }
-    return <FaArrowDown/>
   };
-  
+
+  const renderArrow = () => {
+    if (sorted.reversed) {
+      return <FaArrowUp />;
+    }
+    return <FaArrowDown />;
+  };
+
   const renderUser = () => {
-    return users.map((user) => {
+    return users?.map((user) => {
       return (
         <tr>
-            <td>{user.id}</td>
-            <td>{ `${user.first_name} ${user.last_name}` }</td>
-            <td>{user.ingame}</td>
-            <td>{user.level}</td>
+          <td>{user.id}</td>
+          <td>{user.user}</td>
+          <td>{user.point}</td>
+          <td>
+            {LEVEL_MINE?.find((item) => item.value === user.level)?.label}
+          </td>
         </tr>
       );
     });
   };
-
 
   /*const renderPlayer = () => {
     let listCreate = localStorage.getItem("listUserPlay") ? JSON.parse(localStorage.getItem("listUserPlay")) : []
@@ -97,39 +105,38 @@ function Ranking() {
   return (
     <div className="app">
       <div className="search-container">
-        <input 
-        type = "text" 
-        placeholder="Search..." 
-        value = {searchPhase} 
-        onChange={search}/>
+        <input
+          type="text"
+          placeholder="Search..."
+          value={searchPhase}
+          onChange={search}
+        />
       </div>
       <div className="table-container">
-        <table id='crudTable'>
+        <table id="crudTable">
           <thead>
             <tr>
               <th onClick={sortById}>
-                <span style={{marginRight: 10}}>Id</span>
+                <span style={{ marginRight: 10 }}>Id</span>
                 {sorted.sorted === "id" ? renderArrow() : null}
               </th>
               <th onClick={sortByName}>
-                <span style={{marginRight: 10}}>Name</span>
+                <span style={{ marginRight: 10 }}>Name</span>
                 {sorted.sorted === "name" ? renderArrow() : null}
               </th>
               <th>
-                <span>Ingame</span>
+                <span>Point</span>
               </th>
               <th>
                 <span>Level</span>
               </th>
             </tr>
           </thead>
-          <tbody>
-            {renderUser()}
-          </tbody>
+          <tbody>{renderUser()}</tbody>
         </table>
       </div>
     </div>
-  )
+  );
 }
 
 export default Ranking;
